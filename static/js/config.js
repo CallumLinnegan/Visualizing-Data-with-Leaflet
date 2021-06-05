7877c2de2ad0389b1c908a21736edf4f7dfd0c5b
CallumLinnegan <77475647+CallumLinnegan@users.noreply.github.com>
2021-06-05 14:00:24 +1000

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY2xpbm5lZ2FuIiwiYSI6ImNrcDkxcmVmaDBiaGsydm5yazExMm53ZTMifQ.2mPibpYoZZWvbWgCLTTXyA";
---
+++ static/js/logic2.js
@@ -0,0 +1,123 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+      "<b><p>" + "Magnitude: " + feature.properties.mag + "</p></b>");
+  }
+
+  function pointToLayer(feature, latlng) {
+    return new L.circle(latlng,
+        {radius: iRadius(feature.properties.mag),
+        fillColor: iColor(feature.properties.mag),
+        fillOpacity: .6,
+        color: "#000",
+        stroke: true,
+        weight: .8
+    })
+  }
+  
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: pointToLayer
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+  // Define streetmap and darkmap layers
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+    })
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Light": lightmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [lightmap, earthquakes]
+  });
+
+  var legend = L.control({position: 'bottomleft'});
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    sections = [0, 2, 4, 6],
+    labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < sections.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + iColor(sections[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+            sections[i] + (sections[i + 1] ? '&ndash;' + sections[i + 1] + '<br>' : '+');
+          }
+          return div;
+    };
+          legend.addTo(myMap);
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+}
+
+function iRadius(magnitude) {
+    return magnitude*25000
+}
+
+function iColor(magnitude) {
+    if (magnitude > 6) {
+        return "#FF0000"
+    }
+    else if (magnitude > 4) {
+        return "#FFA500"
+    }
+    else if (magnitude > 2) {
+        return "#FFFF00"
+    }
+    else if (magnitude > 0) {
+        return "#6faf04"
+    }     
+}
+
